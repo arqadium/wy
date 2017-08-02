f8277831f258bd177c0dbea877092f14456f36af
Alexander Nicholi <alex@caveoforig.in>
2017-08-02 05:35:40 -0400

    Write dummy parser + Node class

--- src/parser.py
+++ src/parser.py
@@ -10,8 +10,28 @@
 ## consent of Arqadium.
 ##
 
+from lexer import lex
+
+class Node:
+    children = []
+    def __init__(self, type, match):
+        self.type = type
+        self.text = match.group(0)
+        self.parts = match.groups()
+
+def parse(tokens):
+    return 
+
 def main(args):
-    print('Good morning, Vietnam!')
+    if len(args) == 1:
+        print('Good morning, Vietnam!')
+        return 0
+    from sys import stdout
+    with open(args[1], 'r') as f:
+        tokens = lex(f.read())
+        ast = parse(tokens)
+        # Yet to implement printing of AST
+    print('\nAll done.\n')
     return 0
 
 if __name__ == '__main__':
